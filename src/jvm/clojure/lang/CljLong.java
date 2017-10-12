339157264d62f2b21d69487e767bc159b1ce0b1c
Nathan Fisher <nfisher@junctionbox.ca>
2017-10-12 12:32:17 +0100

    Refactor checked numerics to instance method.
    - Validate extraction to separate class with CljLong.
    - Extract static cast methods to _XXCast instance method.

---
+++ src/jvm/clojure/lang/CljLong.java
@@ -0,0 +1,63 @@
+package clojure.lang;
+
+import java.math.BigInteger;
+
+class CljLong {
+    long longCast(Object x) {
+        if (x instanceof Integer || x instanceof Long) {
+            return ((Number) x).longValue();
+        } else if (x instanceof BigInt) {
+            BigInt bi = (BigInt) x;
+            if (bi.bipart == null) {
+                return bi.lpart;
+            } else {
+                throw new IllegalArgumentException("Value out of range for long: " + x);
+            }
+        } else if (x instanceof BigInteger) {
+            BigInteger bi = (BigInteger) x;
+            if (bi.bitLength() < 64) {
+                return bi.longValue();
+            } else {
+                throw new IllegalArgumentException("Value out of range for long: " + x);
+            }
+        } else if (x instanceof Byte || x instanceof Short) {
+            return ((Number) x).longValue();
+        } else if (x instanceof Ratio) {
+            return longCast(((Ratio) x).bigIntegerValue());
+        } else if (x instanceof Character) {
+            return longCast(((Character) x).charValue());
+        } else {
+            return longCast(((Number) x).doubleValue());
+        }
+    }
+
+    long longCast(byte x) {
+        return x;
+    }
+
+    long longCast(short x) {
+        return x;
+    }
+
+    long longCast(int x) {
+        return x;
+    }
+
+    long longCast(float x) {
+        if (x < Long.MIN_VALUE || x > Long.MAX_VALUE) {
+            throw new IllegalArgumentException("Value out of range for long: " + x);
+        }
+        return (long) x;
+    }
+
+    long longCast(long x) {
+        return x;
+    }
+
+    long longCast(double x) {
+        if (x < Long.MIN_VALUE || x > Long.MAX_VALUE) {
+            throw new IllegalArgumentException("Value out of range for long: " + x);
+        }
+        return (long) x;
+    }
+}
